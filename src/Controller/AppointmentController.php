8d77a6d3fc4056f364b3f41692d7497010f1101a
OlivierDLMR <odlmr@yahoo.fr>
2020-01-15 16:49:44 +0100

    gestion de la sécurité

--- src/Controller/AppointmentController.php
+++ src/Controller/AppointmentController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Appointment;
 use App\Form\AppointmentType;
 use App\Repository\AppointmentRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,9 +18,11 @@ class AppointmentController extends AbstractController
 {
     /**
      * @Route("/", name="appointment_index", methods={"GET"})
+     * @IsGranted("ROLE_ADMIN")
      */
     public function index(AppointmentRepository $appointmentRepository): Response
     {
+
         return $this->render('appointment/index.html.twig', [
             'appointments' => $appointmentRepository->findAll(),
         ]);
